0e402d5e456b69093c324685b586c6f8ed0006f9
Arshad <arshadchowdhury46@gmail.com>
2025-06-03 22:20:04 +0600

    Added example env to demonstrate how to setup envlocal

--- app/components/AdditionalInfoLegend.tsx
+++ app/components/AdditionalInfoLegend.tsx
@@ -17,30 +17,6 @@ const AdditionalInfoLegend = () => {
 
     if (isError) return <p>Error loading legend data</p>;
 
-    // return (
-    //     <div className="mt-6 rounded border border-gray-200 p-4"> {/* Outer container with border and padding */}
-
-    //         {/* Header Row */}
-    //         <div className="grid grid-cols-4 gap-x-6 text-sm mb-4"> {/* gap-x-6 for horizontal spacing, mb-2 for spacing below headers */}
-    //             <p className="text-gray-500 font-semibold uppercase">November 2021</p> {/* Added font-semibold and uppercase for headers */}
-    //             <p className="text-gray-500 font-semibold uppercase">Median</p>
-    //             <p className="text-gray-500 font-semibold uppercase">Rank</p>
-    //             <p className="text-gray-500 font-semibold uppercase">Percentile</p>
-    //         </div>
-
-    //         {/* Separator Line */}
-    //         <hr className="border-t border-gray-400 my-2" /> {/* A simple horizontal rule */}
-
-    //         {/* Data Row */}
-    //         <div className="grid grid-cols-4 gap-x-6 text-sm mt-2"> {/* mt-2 for spacing above data */}
-    //             <p className="font-medium">$328,397</p>
-    //             <p className="font-medium">$373,063</p>
-    //             <p className="font-medium">5 / 68</p>
-    //             <p className="font-medium">94%</p>
-    //         </div>
-    //     </div>
-    // )
-
     const { header, data: legend } = legendData;
 
     return (
